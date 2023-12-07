027de656b37c7d85c7b14df3340d2bf65268a0f1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-06 22:57:11 -0500

    first commit of files

---
+++ android/src/com/venoblin/hoverorb/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.venoblin.hoverorb;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.venoblin.hoverorb.HoverOrb;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HoverOrb(), config);
+	}
+}
