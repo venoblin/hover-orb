848626a21d66857b3f1deed51a88020467e89efa
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-10 00:20:44 -0500

    set up main class

--- core/src/com/venoblin/hoverorb/HoverOrb.java
+++ core/src/com/venoblin/hoverorb/HoverOrb.java
@@ -1,9 +1,11 @@
 package com.venoblin.hoverorb;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 import ball.BallActor;
 
@@ -11,20 +13,28 @@ public class HoverOrb extends ApplicationAdapter {
 
 	private Stage stage;
 	private BallActor ball;
-	private Texture img;
+	private Texture ballTexture;
 
 	@Override
 	public void create () {
-		ball = new BallActor(new Texture("ball.png"));
+		stage = new Stage(new StretchViewport(
+				Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight()
+		));
+		ballTexture = new Texture("ball.png");
+		ball = new BallActor(ballTexture);
+		stage.addActor(ball);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		stage.act();
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
-		img.dispose();
+		ballTexture.dispose();
 	}
 }
