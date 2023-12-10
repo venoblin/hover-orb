41844dedaab377ad2cef690f65cdb61678cac142
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-10 00:01:27 -0500

    added act method

--- core/src/ball/Ball.java
+++ core/src/ball/Ball.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Ball extends Actor {
-    Texture texture;
+    private Texture texture;
 
     Ball(Texture texture) {
         this.texture = texture;
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
     }
 }
