807d45f0ae0768f8cda9c7941817e599dc6b281e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-09 23:47:49 -0500

    ball extends actor and takes in a texture

---
+++ core/src/ball/Ball.java
@@ -0,0 +1,12 @@
+package ball;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Ball extends Actor {
+    Texture texture;
+
+    Ball(Texture texture) {
+        this.texture = texture;
+    }
+}
