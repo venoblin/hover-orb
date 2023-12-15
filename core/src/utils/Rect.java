fc7f7edfc143514fe654002eecc63a636b3a871e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-14 21:05:18 -0500

    worked on rect

--- core/src/utils/Rect.java
+++ core/src/utils/Rect.java
@@ -1,9 +1,22 @@
 package utils;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Rect extends Rectangle {
-    public Rect(float x, float y, float width, float height) {
+    private final int width;
+    private final int height;
+    private final Vector2 position;
+
+    public Rect(float x, float y, int width, int height, int padding) {
+        this.width = width;
+        this.height = height;
+        position = new Vector2(x, y);
+
         set(x, y, width, height);
+    }
+
+    public void updatePosition() {
+        setPosition(position);
     }
 }
