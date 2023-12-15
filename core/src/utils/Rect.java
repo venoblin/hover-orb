80040ca0e2c0619a6cf3baba5c8ebb8498820bd6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-14 21:17:05 -0500

    removed variable and changed method used

--- core/src/utils/Rect.java
+++ core/src/utils/Rect.java
@@ -1,24 +1,21 @@
 package utils;
 
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 
 public class Rect extends Rectangle {
     private final int width;
     private final int height;
     private final int padding;
-    private final Vector2 position;
 
     public Rect(float x, float y, int width, int height, int padding) {
         this.width = width;
         this.height = height;
         this.padding = padding;
-        position = new Vector2(x, y);
 
         set(x - padding, y - padding, width + (padding * 2), height + (padding * 2));
     }
 
     public void updatePosition(float x, float y) {
-        set(x - padding, y - padding, width + (padding * 2), height + (padding * 2));
+        setPosition(x - padding, y - padding);
     }
 }
