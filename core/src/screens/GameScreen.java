cbd2e6d9b2de0f4a017c767ae190fb4cc84fadc0
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-16 19:59:53 -0500

    created game screen

---
+++ core/src/screens/GameScreen.java
@@ -0,0 +1,47 @@
+package screens;
+
+import com.badlogic.gdx.Screen;
+import com.venoblin.hoverorb.HoverOrb;
+
+public class GameScreen implements Screen {
+    private final HoverOrb game;
+
+    public GameScreen(HoverOrb game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
