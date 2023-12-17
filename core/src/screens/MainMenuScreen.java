ac70d41ee234cf77eb2302f6c457c6dbbe959e85
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-16 23:43:29 -0500

    added start button

--- core/src/screens/MainMenuScreen.java
+++ core/src/screens/MainMenuScreen.java
@@ -1,16 +1,39 @@
 package screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.venoblin.hoverorb.HoverOrb;
 
 public class MainMenuScreen implements Screen {
     private final HoverOrb game;
     private final Stage stage;
+    private final Table table;
+    private final TextButton startBtn;
 
-    public MainMenuScreen(HoverOrb game, Stage stage) {
+    public MainMenuScreen(final HoverOrb game, Stage stage) {
         this.game = game;
         this.stage = stage;
+
+        table = new Table();
+        table.setFillParent(true);
+
+        startBtn = new TextButton("Start", new Skin(Gdx.files.internal("skins/uiskin.json")));
+        startBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.goToGame();
+            }
+        });
+
+        table.add(startBtn).size(300, 200);
+
+        stage.addActor(table);
     }
 
     @Override
