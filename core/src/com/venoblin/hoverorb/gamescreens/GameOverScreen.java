aa6786b58d6f5c1ad8d70916e4edb588c9432b2a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-23 14:08:51 -0500

    added listeners

--- core/src/com/venoblin/hoverorb/gamescreens/GameOverScreen.java
+++ core/src/com/venoblin/hoverorb/gamescreens/GameOverScreen.java
@@ -1,12 +1,40 @@
 package com.venoblin.hoverorb.gamescreens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.venoblin.hoverorb.HoverOrb;
 import com.venoblin.hoverorb.screen.ScreenHandler;
 
 public class GameOverScreen extends ScreenHandler {
+    private final TextButton retryBtn;
+    private final TextButton mainMenuBtn;
+
     public GameOverScreen(HoverOrb game, Stage stage) {
         super(game, stage);
+
+        ui.setFillParent(true);
+
+        retryBtn = new TextButton("Retry", new Skin(Gdx.files.internal("skins/uiskin.json")));
+        retryBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+        mainMenuBtn = new TextButton("Main Menu", new Skin(Gdx.files.internal("skins/uiskin.json")));
+        mainMenuBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+
+        ui.add(retryBtn);
+        ui.add(mainMenuBtn);
     }
 
     @Override
