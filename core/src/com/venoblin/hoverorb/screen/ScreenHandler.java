9d052a4cacc6f43c0de20c8fe5e4e8bcd071662a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-23 11:51:22 -0500

    cretaed screen handler class

---
+++ core/src/com/venoblin/hoverorb/screen/ScreenHandler.java
@@ -0,0 +1,44 @@
+package com.venoblin.hoverorb.screen;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class ScreenHandler extends ScreenAdapter {
+    public ScreenHandler() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
