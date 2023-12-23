371493937392d267bf63dfea5ad9c27d3f09eb38
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-23 14:03:25 -0500

    created game over screen

--- core/src/com/venoblin/hoverorb/gamescreens/GameOverScreen.java
+++ core/src/com/venoblin/hoverorb/gamescreens/GameOverScreen.java
@@ -1,7 +1,46 @@
 package com.venoblin.hoverorb.gamescreens;
 
-public class GameOverScreen {
-    public GameOverScreen() {
-        
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.venoblin.hoverorb.HoverOrb;
+import com.venoblin.hoverorb.screen.ScreenHandler;
+
+public class GameOverScreen extends ScreenHandler {
+    public GameOverScreen(HoverOrb game, Stage stage) {
+        super(game, stage);
     }
- }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
