2384f12076c44a3ab4b6739dd177e64ab4c9fe27
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-25 16:49:15 -0500

    created theme screen

---
+++ core/src/com/venoblin/hoverorb/gamescreens/ThemeScreen.java
@@ -0,0 +1,11 @@
+package com.venoblin.hoverorb.gamescreens;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.venoblin.hoverorb.HoverOrb;
+import com.venoblin.hoverorb.screen.ScreenHandler;
+
+public class ThemeScreen extends ScreenHandler {
+    public ThemeScreen(HoverOrb game, Stage stage) {
+        super(game, stage);
+    }
+}
