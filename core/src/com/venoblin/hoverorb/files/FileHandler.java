284a8eaa554fe6511d1b304a842240fc4958ad38
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-26 23:25:11 -0500

    created file handler

---
+++ core/src/com/venoblin/hoverorb/files/FileHandler.java
@@ -0,0 +1,9 @@
+package com.venoblin.hoverorb.files;
+
+import com.badlogic.gdx.files.FileHandle;
+
+public class FileHandler extends FileHandle {
+    public FileHandler(String path) {
+        
+    }
+}
