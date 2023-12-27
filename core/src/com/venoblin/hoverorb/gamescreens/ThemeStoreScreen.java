37b67818c9f122dbd1c385a0d533cec489a3640b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-12-26 19:20:32 -0500

    started work on themestore balls

--- core/src/com/venoblin/hoverorb/gamescreens/ThemeStoreScreen.java
+++ core/src/com/venoblin/hoverorb/gamescreens/ThemeStoreScreen.java
@@ -1,19 +1,26 @@
 package com.venoblin.hoverorb.gamescreens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.venoblin.hoverorb.HoverOrb;
 import com.venoblin.hoverorb.screen.ScreenHandler;
 
 public class ThemeStoreScreen extends ScreenHandler {
+    private final Table ballsTable;
     private final TextButton okBtn;
 
     public ThemeStoreScreen(final HoverOrb game, final Stage stage) {
         super(game, stage);
+
+        ballsTable = new Table();
 
         okBtn = new TextButton("Ok", new Skin(Gdx.files.internal("skins/uiskin.json")));
         okBtn.addListener(new ClickListener() {
@@ -22,6 +29,17 @@ public class ThemeStoreScreen extends ScreenHandler {
                 mainMenu();
             }
         });
+
+        FileHandle ballsHandle = Gdx.files.internal("balls");
+        if (ballsHandle.isDirectory()) {
+            FileHandle[] balls = ballsHandle.list();
+
+            for (FileHandle ball : balls) {
+                Texture ballTexture = new Texture(ball);
+                Image ballImg = new Image(ballTexture);
+                ballsTable.add(ballImg);
+            }
+        }
 
         ui.setFillParent(true);
 
